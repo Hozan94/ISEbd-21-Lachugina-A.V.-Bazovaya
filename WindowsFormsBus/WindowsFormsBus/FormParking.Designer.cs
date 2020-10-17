d24be402be64eac27294faa005f2514490730bb3
Анастасия Лачугина <cool.lachugina@mail.ru>
2020-10-17 21:55:59 +0400

    2

---
+++ WindowsFormsBus/WindowsFormsBus/FormParking.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsBus
+{
+    partial class FormParking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxParking = new System.Windows.Forms.PictureBox();
+            this.buttonSetBus = new System.Windows.Forms.Button();
+            this.buttonSetGarmBus = new System.Windows.Forms.Button();
+            this.groupBoxTakingBus = new System.Windows.Forms.GroupBox();
+            this.buttonTakeBus = new System.Windows.Forms.Button();
+            this.maskedTextBoxNumber = new System.Windows.Forms.MaskedTextBox();
+            this.labelPlace = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
+            this.groupBoxTakingBus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxParking
+            // 
+            this.pictureBoxParking.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBoxParking.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxParking.Name = "pictureBoxParking";
+            this.pictureBoxParking.Size = new System.Drawing.Size(1141, 601);
+            this.pictureBoxParking.TabIndex = 0;
+            this.pictureBoxParking.TabStop = false;
+            // 
+            // buttonSetBus
+            // 
+            this.buttonSetBus.Location = new System.Drawing.Point(1203, 21);
+            this.buttonSetBus.Name = "buttonSetBus";
+            this.buttonSetBus.Size = new System.Drawing.Size(120, 51);
+            this.buttonSetBus.TabIndex = 1;
+            this.buttonSetBus.Text = "Припарковать автобус";
+            this.buttonSetBus.UseVisualStyleBackColor = true;
+            this.buttonSetBus.Click += new System.EventHandler(this.buttonSetBus_Click);
+            // 
+            // buttonSetGarmBus
+            // 
+            this.buttonSetGarmBus.Location = new System.Drawing.Point(1203, 78);
+            this.buttonSetGarmBus.Name = "buttonSetGarmBus";
+            this.buttonSetGarmBus.Size = new System.Drawing.Size(120, 64);
+            this.buttonSetGarmBus.TabIndex = 2;
+            this.buttonSetGarmBus.Text = "Припарковать автобус с гармошкой";
+            this.buttonSetGarmBus.UseVisualStyleBackColor = true;
+            this.buttonSetGarmBus.Click += new System.EventHandler(this.buttonSetGarmBus_Click_1);
+            // 
+            // groupBoxTakingBus
+            // 
+            this.groupBoxTakingBus.Controls.Add(this.buttonTakeBus);
+            this.groupBoxTakingBus.Controls.Add(this.maskedTextBoxNumber);
+            this.groupBoxTakingBus.Controls.Add(this.labelPlace);
+            this.groupBoxTakingBus.Location = new System.Drawing.Point(1203, 148);
+            this.groupBoxTakingBus.Name = "groupBoxTakingBus";
+            this.groupBoxTakingBus.Size = new System.Drawing.Size(150, 109);
+            this.groupBoxTakingBus.TabIndex = 3;
+            this.groupBoxTakingBus.TabStop = false;
+            this.groupBoxTakingBus.Text = "Забрать автобус";
+            // 
+            // buttonTakeBus
+            // 
+            this.buttonTakeBus.Location = new System.Drawing.Point(17, 64);
+            this.buttonTakeBus.Name = "buttonTakeBus";
+            this.buttonTakeBus.Size = new System.Drawing.Size(103, 30);
+            this.buttonTakeBus.TabIndex = 2;
+            this.buttonTakeBus.Text = "Забрать";
+            this.buttonTakeBus.UseVisualStyleBackColor = true;
+            this.buttonTakeBus.Click += new System.EventHandler(this.buttonTakeBus_Click_1);
+            // 
+            // maskedTextBoxNumber
+            // 
+            this.maskedTextBoxNumber.Location = new System.Drawing.Point(73, 36);
+            this.maskedTextBoxNumber.Name = "maskedTextBoxNumber";
+            this.maskedTextBoxNumber.Size = new System.Drawing.Size(47, 22);
+            this.maskedTextBoxNumber.TabIndex = 1;
+            // 
+            // labelPlace
+            // 
+            this.labelPlace.AutoSize = true;
+            this.labelPlace.Location = new System.Drawing.Point(14, 36);
+            this.labelPlace.Name = "labelPlace";
+            this.labelPlace.Size = new System.Drawing.Size(53, 17);
+            this.labelPlace.TabIndex = 0;
+            this.labelPlace.Text = "Место:";
+            // 
+            // FormParking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 601);
+            this.Controls.Add(this.groupBoxTakingBus);
+            this.Controls.Add(this.buttonSetGarmBus);
+            this.Controls.Add(this.buttonSetBus);
+            this.Controls.Add(this.pictureBoxParking);
+            this.Name = "FormParking";
+            this.Text = "Автовокзал";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).EndInit();
+            this.groupBoxTakingBus.ResumeLayout(false);
+            this.groupBoxTakingBus.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxParking;
+        private System.Windows.Forms.Button buttonSetBus;
+        private System.Windows.Forms.Button buttonSetGarmBus;
+        private System.Windows.Forms.GroupBox groupBoxTakingBus;
+        private System.Windows.Forms.Button buttonTakeBus;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxNumber;
+        private System.Windows.Forms.Label labelPlace;
+    }
+}
---
+++ WindowsFormsBus/WindowsFormsBus/FormParking.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsBus
+{
+    public partial class FormParking : Form
+    {
+        private readonly Parking<EasyBus> parking;
+        public FormParking()
+        {
+            InitializeComponent();
+            parking = new Parking<EasyBus>(pictureBoxParking.Width, pictureBoxParking.Height);
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            parking.Draw(gr);
+            pictureBoxParking.Image = bmp;
+        }
+
+        private void buttonSetBus_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var bus = new Bus(100, 1000, dialog.Color);
+                if (parking + bus)
+                {
+                    Draw();
+                }
+                else
+                {
+                    MessageBox.Show("Парковка переполнена");
+                }
+            }
+        }
+
+        private void buttonSetGarmBus_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == DialogResult.OK)
+                {
+                    var bus = new BusGarm(100, 10000, dialog.Color, dialogDop.Color, true, true);
+                    if (parking + bus)
+                    {
+                        Draw();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Автовокзал переполнен");
+                    }
+                }
+            }
+        }
+
+        private void buttonTakeBus_Click_1(object sender, EventArgs e)
+        {
+            if (maskedTextBoxNumber.Text != "")
+            {
+                var bus = parking - Convert.ToInt32(maskedTextBoxNumber.Text);
+                if (bus != null)
+                {
+                    FormBus form = new FormBus();
+                    form.SetBus(bus);
+                    form.ShowDialog();
+                }
+                Draw();
+            }
+        }
+    }
+}
+    
